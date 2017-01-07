ad232ba73e52c3a56f0af50bf9cb76191c914edc
Caspar Friedrich <c.s.w.friedrich@gmail.com>
2017-01-07 11:17:48 +0100

    Add gpio class

--- nRF24L01/App/app.cpp
+++ nRF24L01/App/app.cpp
@@ -1,3 +1,9 @@
+#include <stm32f3xx_hal.h>
+
+#include "gpio.hpp"
 #include "radiotask.hpp"
+
+static Gpio nRF24L01_CE(GPIOF, GPIO_PIN_2);
+static Gpio nRF24L01_IRQ(GPIOC, GPIO_PIN_2);
 
 static RadioTask radioTask;
---
+++ nRF24L01/App/gpio.cpp
@@ -0,0 +1,52 @@
+#include <stm32f3xx_hal.h>
+
+#include <xXx/interfaces/igpio.hpp>
+
+#include "gpio.hpp"
+
+Gpio::Gpio(GPIO_TypeDef *port, uint16_t pin) : _port(port), _pin(pin) {}
+
+Gpio::~Gpio() {}
+
+void Gpio::clear() {
+    HAL_GPIO_WritePin(_port, _pin, GPIO_PIN_RESET);
+}
+
+bool Gpio::get() {
+    return ((bool)HAL_GPIO_ReadPin(_port, _pin));
+}
+
+void Gpio::toggle() {
+    HAL_GPIO_TogglePin(_port, _pin);
+}
+
+void Gpio::set() {
+    HAL_GPIO_WritePin(_port, _pin, GPIO_PIN_SET);
+}
+
+void Gpio::disableInterrupt() {
+    _callback[_pin] = NULL;
+    _user[_pin]     = NULL;
+}
+
+void Gpio::enableInterrupt(IGpio_Callback_t callback, void *user) {
+    if (_callback[_pin]) {
+        return;
+    }
+
+    _callback[_pin] = callback;
+    _user[_pin]     = user;
+}
+
+IGpio_Callback_t Gpio::_callback[16];
+void *Gpio::_user[16];
+
+void Gpio::callback(uint16_t pin) {
+    if (_callback[pin]) {
+        _callback[pin](_user[pin]);
+    }
+}
+
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
+    Gpio::callback(GPIO_Pin);
+}
